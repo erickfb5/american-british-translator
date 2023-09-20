0b05701358138fb2f0e1a44e871a85fd5f50c176
Erick Brandao <erickfb56@gmail.com>
2023-09-20 12:49:06 -0300

    added: constants

---
+++ constants/americanToBritishTitles.js
@@ -0,0 +1,10 @@
+const americanToBritishTitles = {
+    "mr.": "mr",
+    "mrs.": "mrs",
+    "ms.": "ms",
+    "mx.": "mx",
+    "dr.": "dr",
+    "prof.": "prof",
+  };
+  
+  module.exports = { americanToBritishTitles };
---
+++ constants/index.js
@@ -0,0 +1,6 @@
+const { americanOnly } = require("./americanOnly.js");
+const { americanToBritishSpelling } = require("./americanToBritishSpelling.js");
+const { americanToBritishTitles } = require("./americanToBritishTitles.js");
+const { britishOnly } = require("./britishOnly.js");
+
+module.exports = { americanOnly, americanToBritishSpelling, americanToBritishTitles, britishOnly}
